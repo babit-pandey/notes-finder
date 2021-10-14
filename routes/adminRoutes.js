eb7990bc1ffab34bb7414f010cea547342de4723
binamracode <binamraprogrammer@gmail.com>
2021-10-14 18:13:34 +0545

    Added comments for routes to better understand those

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,14 +3,29 @@ const adminControllers = require("../controllers/adminControllers");
 
 const router = express.Router();
 
+// @route  GET admin/
+// @desc   Admin Panel Dashboard
+// @access Administrator
 router.get("/", adminControllers.admin_index);
 
+// @route  GET admin/grades
+// @desc   List of Grades
+// @access Administrator
 router.get("/grades", adminControllers.admin_grades);
 
+// @route  POST admin/update
+// @desc   Update Grades
+// @access Administrator
 router.post("/update/", adminControllers.update_grades);
 
+// @route  POST admin/delete
+// @desc   Delete Grades
+// @access Administrator
 router.post("/delete/", adminControllers.delete_grades);
 
+// @route  POST admin/add
+// @desc   Add Grades
+// @access Administrator
 router.post("/add/", adminControllers.add_grades);
 
 module.exports = router;
